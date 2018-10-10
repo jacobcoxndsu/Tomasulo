9dfa307f34d62f9041bb8e80584356d8d2499003
jacobcoxndsu <jacob.cox@ndsu.edu>
2018-10-10 17:20:30 -0500

    Started Simulator Constructor
    Added the Cycles and instructionRecord construction.

--- src/Simulator.java
+++ src/Simulator.java
@@ -1,14 +1,36 @@
 
 public class Simulator {
-
-	public Simulator() {
-		
-	}
 	
-	static InstructionRecord[] instructionRecords;
-	static int[] registerFile;
-	static int[] RAT;
-	static ReservationStation[] addSubRS;
-	static ReservationStation[] multDivRS;
+	private int cycles;
+	private InstructionRecord[] instructionRecords;
+	private int[] registerFile;
+	private int[] RAT;
+	private ReservationStation[] addSubRS;
+	private ReservationStation[] multDivRS;
+
+	public Simulator(int cy, int[] ir, int[] rg) {
+		
+		//cycles
+		cycles = cy;
+		
+		//instructionRecords
+		instructionRecords = new InstructionRecord[ir.length];
+		for(int i = 0; i < ir.length; i++){
+			int opcode = Integer.toString(ir[i]).charAt(0);
+			int destOp = Integer.toString(ir[i]).charAt(1);
+			int sourceOp1 = Integer.toString(ir[i]).charAt(2);
+			int sourceOp2 = Integer.toString(ir[i]).charAt(3);
+			InstructionRecord inst = new InstructionRecord(opcode, destOp, sourceOp1, sourceOp2);
+			instructionRecords[i] = inst;
+		}
+		
+		//registerFile
+		
+		//RAT
+		
+		//addSubRS
+		
+		//multDivRS
+	}
 
 }
