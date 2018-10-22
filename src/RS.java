d11175dafd76e3a2110df944e25a55d5f6e5e29b
jacobcoxndsu <jacob.cox@ndsu.edu>
2018-10-22 17:44:32 -0500

    working on Step for RS

--- src/RS.java
+++ src/RS.java
@@ -8,7 +8,28 @@ public class RS {
 		rs = new int[5][6]; // 5 Reservation Stations
 	}
 	
-	public RS Step(){
-		return new RS();
+	public RS Step(IQ iq){
+		InstructionRecord ir = iq.getCurrentInstruction();
+		
+		if(ir.opcode == 0 || ir.opcode == 1){
+			for(int i = 0; i < rs.length - 2; i++){
+				if(rs[i][6] == 0){
+					//put instruction into the add/subtract slot RS
+					//increment IQ
+					iq.Increment();
+				}
+			}
+		}
+		
+		if(ir.opcode == 2 || ir.opcode == 3){
+			for(int i = 3; i < rs.length; i++){
+				if(rs[i][6] == 0){
+					//put instruction into the multiply/divide slot RS
+					//increment IQ
+					iq.Increment();
+				}
+			}
+		}
+		return this;
 	}
 }
