38e00706cd6fcc7f429d49bb413cb226aa342604
jacobcoxndsu <jacob.cox@ndsu.edu>
2018-11-30 19:35:14 -0600

    fixed up the Printing more

--- src/com/current/Simulator.java
+++ src/com/current/Simulator.java
@@ -415,9 +415,9 @@ public class Simulator {
 			
 			if(robi < rob.length) {
 				if(rf[i] > 9){
-					System.out.print("          RB" + (robi + 1) + ":  " + rob[robi][1]);
+					System.out.print("          RB" + (robi + 1) + ": " + rob[robi][1]);
 				} else {
-					System.out.print("         RB" + (robi + 1) + ":  " + rob[robi][1]);
+					System.out.print("         RB" + (robi + 1) + ": " + rob[robi][1]);
 				}
 				robi++;
 			}
